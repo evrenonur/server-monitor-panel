08a5763331fb09ce07363deeb10e30b67ccdb5be
Onur Evren <onur.evren.1999@gmail.com>
2024-12-27 13:45:56 +0300

    Update ServerSshController to generate and assign API token for the authenticated user
    - Added functionality to generate a random API token for the currently authenticated user when accessing the SSH view.
    - Updated the user's API token in the database to enhance security and session management.
    - Ensured the server's SSH port defaults to 22 if not set, maintaining existing functionality.
    
    This change improves user authentication and security within the admin panel's SSH management feature.

--- app/Http/Controllers/Admin/ServerSshController.php
+++ app/Http/Controllers/Admin/ServerSshController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Server;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class ServerSshController extends Controller
 {
     public function show(Server $server)
     {
-        if (!$server->ssh_port) {
-            $server->ssh_port = 22;
-        }
-
+        $token = Str::random(60);
+        // Şu anki kullanıcıya ait 'api_token' güncelleniyor.
+        User::where('id', auth()->user()->id)->update(['api_token' => $token]);
+        auth()->user()->setAttribute('api_token', $token);
         return view('admin.servers.ssh', [
-            'server' => $server
+            'server' => $server,
+
         ]);
     }
 }
